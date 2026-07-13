165fdbba0bfd44374814b6814554d4ebc1960441
Natalia <npopova@solvd.com>
2026-07-13 23:37:31 +0300

    temp

--- tests/saucedemo.spec.ts
+++ tests/saucedemo.spec.ts
@@ -8,7 +8,7 @@ test.describe("Login", () => {
         await page.goto("/");
     });
 
-    test("Login - Happy path", async ({ page }) => {
+    test.only("Login - Happy path", async ({ page }) => {
         await page.getByPlaceholder("Username").fill("standard_user");
         await page.getByPlaceholder("Password").fill("secret_sauce");
         await page.getByRole("button", { name: "Login" }).click();
